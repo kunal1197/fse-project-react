08b5e30bbe0aaa71e8a2c10cd8456d4f271c8976
Farida Olia <farruolia@gmail.com>
2022-11-23 15:09:08 -0500

    fixed comment button

--- src/components/songs/songStats.js
+++ src/components/songs/songStats.js
@@ -3,11 +3,14 @@
 const SongStats = () => {
 
     return(
-        <div className="align-self-end">
+        <div>
+            <div className="wd-float-left">
+                <i className="fa-regular fa-heart wd-grey-text"></i>
+                <a className="wd-icons-text wd-heart" href=""></a>
+            </div>
             <div className="wd-float-left">
                 <i className="fa-regular fa-comment wd-grey-text"></i>
-                <a className="wd-icons-text" href="">
-                </a>
+                <a className="wd-icons-text" href=""></a>
             </div>
         </div>
     )
